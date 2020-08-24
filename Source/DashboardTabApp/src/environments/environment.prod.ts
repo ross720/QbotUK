7321728c8cc0f02bf674c7e1b66dbc4b71706ab7
System Adminsitrator <69931832+Contoso-uk-edu@users.noreply.github.com>
2020-08-24 15:20:03 -0700

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qboteduuknine.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qboteduukten.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "9e4d7ef0-9135-47fc-b4cd-a6cbc485e0bd",
-        clientId: "23a698fe-ef8e-41b6-a4fc-5227fb09d814",
+        clientId: "b0737784-f15b-490c-bd47-ce99011a70d0",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
