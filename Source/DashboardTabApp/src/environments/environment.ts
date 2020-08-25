464196a912659c8d02647a1ea88d5f4c3091cac1
System Adminsitrator <69931832+Contoso-uk-edu@users.noreply.github.com>
2020-08-24 18:08:51 -0700

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotukcontosouni.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotukcontosotwo.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
